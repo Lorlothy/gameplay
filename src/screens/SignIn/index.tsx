33c91c1fba7ddaa62eefb7e9cc7375db1aa78f5d
Lorlothy <fael.rgt@gmail.com>
2021-07-30 07:20:11 -0300

    changed fonts

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,7 +3,6 @@ import {
     View,
     Text, 
     Image, 
-    StatusBar 
 } from 'react-native'
 
 import { ButtonIcon } from '../../components/ButtonIcon'
@@ -13,11 +12,7 @@ import { styles } from './styles'
 export function SignIn(){
     return(
         <View style={styles.container}>
-            <StatusBar 
-                barStyle="light-content"
-                backgroundColor="transparent"
-                translucent
-            />
+            
 
             <Image 
               source={IllustrationImg}
@@ -27,9 +22,9 @@ export function SignIn(){
             />
             <View style={styles.content}>
                 <Text style={styles.title}>
-                    Organize{`\n`}
-                    suas jogatinas{`\n`}
-                    facilmente
+                    Conecte-se{`\n`}
+                    e organize suas{`\n`}
+                    jogatinas
                 </Text>
 
                 <Text style={styles.subtitle}>
